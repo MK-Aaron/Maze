449e8b937c03b167823839ea8545a1c8a5717c8a
MK-Aaron <mwendwa_aaron78@yahoo.com>
2024-09-10 18:26:59 +0300

    Rebase

--- src/main.c
+++ src/main.c
@@ -1,98 +1,128 @@
 #include <stdio.h>
-#include <SDL2/SDL.h>
-#include "../headers/constants.h"
+#include <stdint.h>
+#include <stdbool.h>
+#include "../headers/defs.h"
+#include "../headers/graphics.h"
+#include "../headers/map.h"
+#include "../headers/player.h"
+#include "../headers/ray.h"
+#include "../headers/wall.h"
+#include "../headers/sprite.h"
+#include "../headers/textures.h"
 
-int game_is_running = FALSE;
-SDL_Window* window = NULL;
-SDL_Renderer* renderer = NULL;
+bool	isGameRunning = false;
+int		ticksLastFrame;
 
-int initialize_window(void)
+void	setup(void)
 {
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
-	{
-		fprintf(stderr, "Error intializing SDL.\n");
-		return (0);
-	}
-	window = SDL_CreateWindow(
-		NULL,
-		SDL_WINDOWPOS_CENTERED,
-		SDL_WINDOWPOS_CENTERED,
-		WINDOW_WIDTH,
-		WINDOW_HEIGHT,
-		SDL_WINDOW_BORDERLESS
-	);
-	if (!window)
-	{
-		fprintf(stderr, "Error creating SDL Window.\n");
-		return (0);
-	}
-	renderer = SDL_CreateRenderer(window, -1, 0);
-	if (!renderer)
-	{
-		fprintf(stderr, "Error creating SDL renderer.\n");
-		return (0);
-	}
-	return (1);
+	// Asks uPNG library to decode all PNG files and loads the wallTextures array
+	loadTextures();
 }
 
-void process_input(void)
+void	processInput(void)
 {
-	SDL_Event event;
+	SDL_Event	event;
 
 	SDL_PollEvent(&event);
-
 	switch (event.type)
 	{
 		case SDL_QUIT:
-			game_is_running = FALSE;
+		{
+			isGameRunning = false;
 			break;
-
+		}
 		case SDL_KEYDOWN:
+		{
 			if (event.key.keysym.sym == SDLK_ESCAPE)
-				game_is_running = FALSE;
+				isGameRunning = false;
+			if (event.key.keysym.sym == SDLK_UP)
+				player.walkDirection = +1;
+			if (event.key.keysym.sym == SDLK_DOWN)
+				player.walkDirection = -1;
+			if (event.key.keysym.sym == SDLK_RIGHT)
+				player.turnDirection = +1;
+			if (event.key.keysym.sym == SDLK_LEFT)
+				player.turnDirection = -1;
 			break;
+		}
+		case SDL_KEYUP:
+		{
+			if (event.key.keysym.sym == SDLK_ESCAPE)
+				isGameRunning = false;
+			if (event.key.keysym.sym == SDLK_UP)
+				player.walkDirection = 0;
+			if (event.key.keysym.sym == SDLK_DOWN)
+				player.walkDirection = 0;
+			if (event.key.keysym.sym == SDLK_RIGHT)
+				player.turnDirection = 0;
+			if (event.key.keysym.sym == SDLK_LEFT)
+				player.turnDirection = 0;
+			break;
+		}
 	}
 }
 
-void setup(void)
+void	update(void)
 {
+	int		timeToWait;
+	float	deltaTime;
+
+	// Compute how long we have until the reach the target frame time in milliseconds
+	timeToWait = FRAME_TIME_LENGTH - (SDL_GetTicks() - ticksLastFrame);
+
+	// Only delay execution if we are running too fast
+	if (timeToWait > 0 && timeToWait <= FRAME_TIME_LENGTH) {
+		SDL_Delay(timeToWait);
+	}
+
+	// Compute the delta time to be used as an update factor when changing game objects
+	deltaTime = (SDL_GetTicks() - ticksLastFrame) / 1000.0f;
+
+	// Store the milliseconds of the current frame to be used in the future
+	ticksLastFrame = SDL_GetTicks();
+
+	movePlayer(deltaTime);
+	castAllRays();
 }
 
-void update(void)
+void	render(void)
 {
-	// todo
+	// clear the color buffer
+	clearColorBuffer(0xFF000000);
+
+	// Render the wall and sprites
+	renderWallProjection();
+	renderSpriteProjection();
+
+	// Render the minimap objects
+	// display the minimap
+	renderMapGrid();
+	renderMapRays();
+	renderMapSprites();
+	renderMapPlayer();
+
+	renderColorBuffer();
 }
 
-void render(void)
+void	releaseResources(void)
 {
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-	SDL_RenderClear(renderer);
-
-	/*Game objects*/
-	SDL_RenderPresent(renderer);
-
-
+	freeTextures();
+	destroyWindow();
 }
 
-void destroy_window(void)
+int	main(void)
 {
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
-}
-int main(void)
-{
-	game_is_running = initialize_window();
+	isGameRunning = initializeWindow();
 
 	setup();
 
-	while (game_is_running)
-	{
-		process_input();
+	while (isGameRunning) {
+		processInput();
 		update();
 		render();
 	}
-	destroy_window();
 
-	return (0);
+	releaseResources();
+
+	return (EXIT_SUCCESS);
 }
